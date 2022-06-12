a16a2f65266a755dc55b9c117eeb19e901f55943
danilobml <danbarml@gmail.com>
2022-06-12 10:59:43 +0200

    style adjustments author-card

--- src/components/AuthorCard.js
+++ src/components/AuthorCard.js
@@ -4,12 +4,12 @@ import "./PostCard.css";
 
 const AuthorCard = ({ author }) => {
   return (
-    <Card border="0" style={{ width: "30rem", height: "200px" }}>
+    <Card border="0" style={{ width: "30rem", maxHeight: "200px" }} className="author-card">
       <Link to={`/author/${author.sys.id}`} className="card-links">
         <Card.Img style={{ height: "100px", width: "100px" }} variant="top" src={author.fields.authorpic.fields.file.url} />
         <Card.Body>
           <Card.Title>{author.fields.name}</Card.Title>
-          <Card.Text>{author.fields.slug}</Card.Text>
+          <Card.Text className="d-none d-sm-block">{author.fields.slug}</Card.Text>
         </Card.Body>
       </Link>
     </Card>
